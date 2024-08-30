2ecf22adf24ceca07a366b072cc89f15cb3e0c60
Mkhgkk <emmachalz745@outlook.com>
2024-08-30 18:00:25 +0900

    feat: streaming from socket

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+"use client"
+
 import { cookies } from "next/headers"
 import Image from "next/image"
 
 import { Monitor } from "../components/monitor"
 
 export default function MonitoringPage() {
-  const layout = cookies().get("react-resizable-panels:layout")
-  const collapsed = cookies().get("react-resizable-panels:collapsed")
+  // const layout = cookies().get("react-resizable-panels:layout")
+  // const collapsed = cookies().get("react-resizable-panels:collapsed")
 
   // const defaultLayout = layout ? JSON.parse(layout.value) : undefined
   // const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined
